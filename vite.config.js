bc4d37c9f4a80f3b01ea3b3e85ae95e1078bd591
Will-X3 <133908961+Will-X3@users.noreply.github.com>
2026-06-21 09:45:39 -0700

    Auto Sitemap Created

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import sitemap from "vite-plugin-sitemap";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://invari.app",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
